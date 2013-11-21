1adc33fcd11e048194326a716dc36cf95472cc54
Ferenc Grecu <mivor20@gmail.com>
2013-11-21 13:19:28 +0200

    Initial commit`

---
+++ Merkell-Hellman-Encryption/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merkell_Hellman_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get Msg
+            //Generate MH pub-priv key pair
+            //Encrypt Message
+            //Decrypt Message
+            //Display Msg
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+## Implement Alg. Merkell-Hellman
+
+* Gen super inc subset
+* Sol to Super inc subset problem
+* Generate Key
+* Encrypt Msg
+* Decrypt Msg
+* ??Multiple Iterated
+
+## Classes:
+
+### MHCryptoProvider
+
+* Encrypt
+* Decrypt
+* KeyGen
+
+### MHKeyPair
+
+* Generate
+
+### SupIncSeq
+
+* Generate
+* Solve
+
+## Knapsack problem:
+
+* Given N integers
+* And a number S
+* Submultime a lui N ca suma sa fie S!
